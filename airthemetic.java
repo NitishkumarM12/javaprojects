f3d3662ef87b02b36d596401cb6263f9fbc3b38c
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-04-11 10:18:40 +0530

    save file

---
+++ airthemetic.java
@@ -0,0 +1,12 @@
+public class airthemetic {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        System.out.println("sum is "+(x+y));
+        System.out.println("difference is "+(x-y));
+        System.out.println("product is "+(x*y));
+        System.out.println("division is "+(x/y));
+        System.out.println("modulus is "+(x%y));
+    }
+}
